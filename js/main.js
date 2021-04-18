971634831f5cb56ff516208853ef5ba99a788b00
Uziel Hernandez <uzielmelitonh@gmail.com>
2021-04-18 18:08:14 -0500

    Dark mode

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-const $menu = document.querySelector('.drawer__menu');
-const $menuIcon = document.querySelector('.header__menu-icon');
-const $menuClose = document.querySelector('.drawer__button--close');
+function $(selector){
+    return document.querySelector(selector);
+}
+const $menu = $('.drawer__menu');
+const $menuIcon = $('.header__menu-icon');
+const $menuClose = $('.drawer__button--close');
+const $swithTheme = $('.header__btn-theme');
+const $iconTheme = $('.theme__dark');
 
 $menuIcon.addEventListener('click', showOrHiddenMenuDrawer );
 $menuClose.addEventListener('click', showOrHiddenMenuDrawer );
 
+$swithTheme.addEventListener('click', () => {   
+    const isLigthMode = document.documentElement.getAttribute('data-theme') === 'ligth';
+    document.documentElement.setAttribute('data-theme', isLigthMode? 'dark' : 'ligth');    
+    $iconTheme.classList.toggle('theme__ligth');
+} );
+
+
 function showOrHiddenMenuDrawer(){
     $menu.classList.toggle('drawer__menu--close');
 }
+
+
+
+
+
+
+
